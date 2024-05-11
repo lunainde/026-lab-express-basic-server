7b9bda67682bf352935b6574f3f2edfd43c1a6a6
lunainde <lunainde@gmail.com>
2024-05-11 09:35:15 +0200

    iteration 1-7

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
+//install Run npm init -y | npm install express |  npm install morgan
 // IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morgan = require("morgan");
 
 // CREATE EXPRESS APP
-// Here you should create your Express app:
+const app = express();
 
-
+// Import projects JSON data
+const projects = require('./data/projects.json');
+const articles = require('./data/articles.json');
 
 // MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-
+app.use(express.static("public"));
+app.use(express.json()); 
+app.use(morgan("dev")); 
 
 // ROUTES
 // Start defining your routes here:
 
-
-
 // START THE SERVER
-// Make your Express server listen on port 5005:
+const port = 5001;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+//Start server auto: npm install nodemon --global
+//npm install nodemon --save-dev
+//add dev to package.json => "scripts" =>  "dev": "nodemon app.js"
+//npm run dev
+//http://localhost:5001
+
+//ROUTERS
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+
+// Import JSON data
+app.get("/api/projects", (req, res) => {
+  res.json(projects);
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/views/not-found.html");
+});
+
